2e54b93867d71b61e6775d2f0cc0378c28ef301c
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-28 12:56:59 +0200

    add: style text inside chat

--- js/myjs.js
+++ js/myjs.js
@@ -2,13 +2,36 @@ const { createApp, ref, onMounted } = Vue;
 
 createApp({
   setup() {
-    const elemento = ref([]);
+    const contacts = ref([
+      {
+          name: 'Michele',
+          avatar: './img/avatar_1.png',
+          visible: true,
+          messages: [
+              {
+                  date: '10/01/2020 15:30:55',
+                  message: 'Hai portato a spasso il cane?',
+                  status: 'sent'
+              },
+              {
+                  date: '10/01/2020 15:50:00',
+                  message: 'Ricordati di stendere i panni',
+                  status: 'sent'
+              },
+              {
+                  date: '10/01/2020 16:15:22',
+                  message: 'Tutto fatto!',
+                  status: 'received'
+              }
+          ],
+      },
+    ])
 
     onMounted(() => {
 });
 
     return {
-        elemento
+      contacts
     };
   }
 }).mount('#app');
